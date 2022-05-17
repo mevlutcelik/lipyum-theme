158a2a1e3abcf31ffda49e6114d5fd4f70cb27da
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-05-17 18:08:50 +0300

    Update home.blade.php
    Updated the customer support button.

--- resources/views/elements/headers/top-content/home.blade.php
+++ resources/views/elements/headers/top-content/home.blade.php
@@ -3,9 +3,12 @@
         <h6 style="line-height: 1.5">Sektörünüz Hiç Fark Etmiyor!<br/>Potansiyel Müşterilere İhtiyacın Varsa,</h6>
         <h2><em>Lipyum</em> Yanında!</h2>
         <div class="white-button">
-            <a href="#customer-support" id="customer-support" style="font-weight: 500">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16" style="width: 1.125rem;height: 1.125rem;margin-right: 0.5rem;"> <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z"></path> </svg>
-                <span>Müşteri Desteği</span>
+            <a href="#customer-support" id="customer-support" style="font-weight: 500;font-size: 1rem;display: inline-flex;align-items: center;">
+                <svg style="width: 1.5rem;height: 1.5rem;margin-right: 1rem;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16" style="width: 1.125rem;height: 1.125rem;margin-right: 0.5rem;"> <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z"></path> </svg>
+                <div style="display: flex;flex-direction: column;justify-content: center;">
+                    <span style="font-size: 0.75rem">0 (850) 308 8170</span>
+                    <span>Müşteri Desteği</span>
+                </div>
             </a>
         </div>
     </div>
