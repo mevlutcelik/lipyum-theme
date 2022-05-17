d2c14dd2d459bd968fffb19fc7f191fa8da938f8
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-05-17 09:20:57 +0300

    The pictures on the homepage have been edited.

--- resources/views/elements/sections/whats-trending.blade.php
+++ resources/views/elements/sections/whats-trending.blade.php
@@ -1,7 +1,7 @@
 <section class="whats-trending" style="margin: 5rem 3rem;">
     <div class="container expanded">
         <div class="row">
-            <div class="col-lg-6 align-self-center">
+            <div class="col-lg-8 align-self-center">
                 <div class="section-heading">
                     <h2>LİPYUM - MÜŞTERİ<br><em>DEĞİŞİM PROGRAMI</em></h2>
                 </div>
@@ -13,16 +13,16 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-4" style="text-align: right;">
                 <div class="right-image">
                     <div class="thumb">
                         <div class="hover-effect">
                             <div class="inner-content">
-                                <h4><a href="#">Modernized Interior</a></h4>
-                                <span>Guest Room Decoration</span>
+                                <h4><a href="#">Çilingir</a></h4>
+                                <span>Bu hafta 940 hizmet verildi.</span>
                             </div>
                         </div>
-                        <img src="{{ asset('images/whats-trending-item-image.jpg')  }}" alt="">
+                        <img style="height: 27rem;width:22rem;object-fit: cover;" src="https://media.istockphoto.com/photos/handyman-repair-the-door-lock-in-the-room-closeup-of-man-repairing-picture-id1309383869?b=1&k=20&m=1309383869&s=170667a&w=0&h=3eS62EB3kpsNOh6TUKtQsb1kw6KHwoWPOZzPF80wvLM=" alt="">
                     </div>
                 </div>
             </div>
