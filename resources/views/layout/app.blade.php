cf3b10e1c4004b087e19653d6dc0621ac8dca0a5
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-05-18 14:11:24 +0300

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -92,9 +92,9 @@
 <script src="{{ asset('js/popup.js') }}"></script>
 <script src="{{ asset('js/custom.js') }}"></script>
 <script>
-    setTimeout(function () {
+    $( document ).ready(function() {
         $('.loader').fadeToggle();
-    }, 1500);
+    });
 
     $("a[href='#top']").click(function () {
         $("html, body").animate({scrollTop: 0}, "slow");
